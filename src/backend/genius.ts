4ed0e130588aa2efc09d65dac2c5c0e905b5a622
Adrian <gogosadrian@outlook.com>
2024-05-18 23:17:11 +0200

    xd

--- src/backend/genius.ts
+++ src/backend/genius.ts
@@ -5,7 +5,7 @@ export default async function SearchSongLyrics(api_key: string, title: string, a
     const response:AxiosResponse = await axios({
         method:'get',
 
-        url: "http://lyricsify.onrender.com/fetch_lyrics",
+        url: "https://lyricsify.onrender.com/fetch_lyrics",
         
         headers:{
             Authorization:api_key
