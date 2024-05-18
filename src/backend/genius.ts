97e779ca67280cc4178b621b714b84e10e866684
Adrian <gogosadrian@outlook.com>
2024-05-18 23:55:29 +0200

    frontend updated

--- src/backend/genius.ts
+++ src/backend/genius.ts
@@ -2,10 +2,11 @@ import axios, {AxiosResponse} from "axios"
 
 
 export default async function SearchSongLyrics(api_key: string, title: string, artist: string ): Promise<string[]>{
+    const backend_url = process.env['REACT_APP_BACKEND_URL'] ?? ""
     const response:AxiosResponse = await axios({
         method:'get',
 
-        url: "https://lyricsify.onrender.com/fetch_lyrics",
+        url: `${backend_url}/fetch_lyrics`,
         
         headers:{
             Authorization:api_key
